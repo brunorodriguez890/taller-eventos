fa20d824f12157ad28b70d9cdf5fb20c8f0900e9
Bruno Rodriguez <bruno.rodriguez2@estudiante.ceibal.edu.uy>
2023-09-02 19:38:30 -0300

    Solucionado el problemita

--- eventos.js
+++ eventos.js
@@ -5,5 +5,11 @@ document.addEventListener("DOMContentLoaded",()=>{
     document.getElementById("divi").addEventListener("click",()=>{
         alert("Hola! soy el div");
     })
+
+    document.getElementById("botonsaludo").addEventListener("click",(e)=>{
+        hola();
+        e.stopPropagation();
+    })
 })
+
 
